d8d5ad9623f81fc16118daaab921244f12569786
mdb1710 <mdb1710@gmail.com>
2020-05-19 21:28:06 -0400

    more changes to index

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="so-wrong-for-that" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -73,7 +73,7 @@ const Navbar = class extends React.Component {
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Media
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
