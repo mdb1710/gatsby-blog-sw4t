6344c94419337c42759556b22390529239b050d6
mdb1710 <mdb1710@gmail.com>
2020-06-01 09:44:16 -0400

    removed intro section from home for now

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
+// import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
@@ -13,7 +13,7 @@ export const IndexPageTemplate = ({
   subheading,
   mainpitch,
   description,
-  intro,
+  
 }) => (
   <div>
     <div
@@ -86,7 +86,7 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
+                
                 
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
